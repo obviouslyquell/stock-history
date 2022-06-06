69e18cd3bbd487c856dfda21c0e2f87f22c5077b
obviouslyquell <obviouslyquell@gmail.com>
2022-06-06 04:34:43 +0300

    add search + search checkbox

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -8,6 +8,7 @@ function Sidebar() {
   const [selectValue, setSelectValue] = useState('Month');
   const [stockInfo, setStockInfo] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const [enableSearch, setEnableSearch] = useState(false);
 
   const key = 'QZIEPYB5LUEZ87YV';
   const url = 'https://www.alphavantage.co/';
@@ -30,6 +31,10 @@ function Sidebar() {
   //   return { ticket: ticket, arr: arr, max: max };
   // }
   function convertForGraph(obj) {
+    if (Object.hasOwn(obj, 'Note')) {
+      alert('5 запросов в минуту');
+      //return null;
+    }
     const ticket = obj['Meta Data']['2. Symbol'];
     const arr = [];
     if (obj['Meta Data']['1. Information'] === 'Monthly Adjusted Prices and Volumes') {
@@ -85,9 +90,12 @@ function Sidebar() {
       default:
         break;
     }
+
     axios
       .get(`${url}query?function=${timeRange}&symbol=${value}&apikey=${key}`)
       .then((data) => setDataValue(convertForGraph(data.data)));
+
+    setValue(''); // сбрасывает инпут
   };
 
   // const getSuggestions = (ticket) => {
@@ -100,13 +108,22 @@ function Sidebar() {
   // };
 
   useEffect(() => {
-    axios
-      .get(
-        `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${value}&apikey=${key}`,
-      )
-      .then((data) => setSuggestions(data.data.bestMatches));
-  }, [value]);
+    if (enableSearch) {
+      axios
+        .get(
+          `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${value}&apikey=${key}`,
+        )
+        .then((data) => setSuggestions(data.data.bestMatches));
+    } else {
+      setSuggestions([]); // hide suggestions
+    }
+  }, [value, enableSearch]);
 
+  const onItemClick = (e) => {
+    console.log(e.target.textContent);
+    setValue(e.target.textContent);
+    document.getElementById('searchBtn').click();
+  };
   const handleChange = (e) => {
     setValue(e.target.value.toUpperCase());
     // if (value.length > 1) {
@@ -118,6 +135,36 @@ function Sidebar() {
   };
   return (
     <header className="header">
+      {/* <input
+        type="checkbox"
+        id="searchCheckBox"
+        checked={enableSearch}
+        onChange={() => setEnableSearch(!enableSearch)}
+      />
+      <label htmlFor="searchCheckBox" className="checkbox__label">
+        Enable search
+      </label> */}
+
+      <label className="toggle" htmlFor="searchCheckBox">
+        <input
+          type="checkbox"
+          className="toggle__input"
+          id="searchCheckBox"
+          checked={enableSearch}
+          onChange={() => setEnableSearch(!enableSearch)}
+        />
+        <span className="toggle-track">
+          <span className="toggle-indicator">
+            <span className="checkMark">
+              <svg viewBox="0 0 24 24" id="ghq-svg-check" role="presentation" aria-hidden="true">
+                <path d="M9.86 18a1 1 0 01-.73-.32l-4.86-5.17a1.001 1.001 0 011.46-1.37l4.12 4.39 8.41-9.2a1 1 0 111.48 1.34l-9.14 10a1 1 0 01-.73.33h-.01z"></path>
+              </svg>
+            </span>
+          </span>
+        </span>
+        Enable search
+      </label>
+
       <form action="submit" className="header__form">
         <input
           id="input"
@@ -135,11 +182,13 @@ function Sidebar() {
           <option value="Week">Week</option>
           <option value="Day">Day</option>
         </select>
-        <button onClick={getData}>Submit</button>
+        <button onClick={getData} id="searchBtn">
+          Submit
+        </button>
       </form>
       {suggestions &&
         suggestions.map((e, index) => (
-          <div key={index} className="search__container">
+          <div key={index} className="search__container" onClick={onItemClick}>
             <span className="search__item-name">{e['1. symbol']}</span>
             <span className="search__item-currency">{e['8. currency']}</span>
           </div>
